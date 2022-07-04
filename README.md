87de1d2774919a29cba1826849075bccc52ba639
yoseph <diddytpq5@gmail.com>
2022-07-04 14:25:50 +0900

    ADD: mecanum lio sam code

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# Mecanum-robot-slam-ws
-mecanum robot slam workspace
+# Mecanum Slam Workspace
+
+## 1. Dependency
+
+### 1.1. pcl ver  
+	https://ropiens.tistory.com/65
+### 1.2 GTSAM
+	https://github.com/diddytpq/LIO-SAM/tree/noetic
+
+## 2. check usb port and port permission 
+	ls /dev/ttyUSB* 
+	sudo chmod 666 /dev/tty*
+	
+## 3. Record topic
+	rosbag record /velodyne_points /tf /imu/data
+	roscore
+	rosparam set /use_sim_time true
+	rosbag play --clock --pause rosbag/*.bag
+
+## 4. Run lio sam (test)
+	roslaunch velodyne_pointcloud VLP16_points.launch
+	roslaunch ros-ngimu run.launch
+
+	roslaunch lio_sam run.launch 
+	rosbag play --clock --pause rosbag/*.bag --topic /velodyne_points /tf /imu/data
+	
+## 5. Run mecanum_robot_control_joy && mecanum of lio sam 
+
+	roslaunch openrobot_control openrobot_control_6omni.launch
+	roslaunch lio_sam run_mecanum.launch
+
+
+## 6. trouble shooting
+* cv_bridge Error
+	```
+	https://jstar0525.tistory.com/118
+	```
+
---
+++ src/imu_pkg/ros-ngimu/include/NgimuReceive.h
@@ -0,0 +1,88 @@
+/**
+ * @file NgimuReceive.h
+ * @author Seb Madgwick
+ * @brief Module for receiving messages from an NGIMU.
+ */
+
+#ifndef NGIMU_RECEIVE_H
+#define NGIMU_RECEIVE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//------------------------------------------------------------------------------
+// Includes
+
+#include "Osc99/Osc99.h"
+
+//------------------------------------------------------------------------------
+// Definitions
+
+/**
+ * @brief Timestamp and argument values for "/sensors" message.
+ */
+typedef struct {
+    OscTimeTag timestamp;
+    float gyroscopeX;
+    float gyroscopeY;
+    float gyroscopeZ;
+    float accelerometerX;
+    float accelerometerY;
+    float accelerometerZ;
+    float magnetometerX;
+    float magnetometerY;
+    float magnetometerZ;
+    float barometer;
+} NgimuSensors;
+
+/**
+ * @brief Timestamp and argument values for "/quaternion" message.
+ */
+typedef struct {
+    OscTimeTag timestamp;
+    float w;
+    float x;
+    float y;
+    float z;
+} NgimuQuaternion;
+
+/**
+ * @brief Timestamp and argument values for "/euler" message.
+ */
+typedef struct {
+    OscTimeTag timestamp;
+    float roll;
+    float pitch;
+    float yaw;
+} NgimuEuler;
+
+/**
+ * @brief Timestamp and argument values for "/temperature" message.
+ */
+typedef struct {
+    OscTimeTag timestamp;
+    float temp1;
+    float temp2;
+} NgimuTemperature;
+
+//------------------------------------------------------------------------------
+// Function prototypes
+
+void NgimuReceiveInitialise();
+void NgimuReceiveSetReceiveErrorCallback(void (*newReceiveErrorCallback)(const char* const errorMessage));
+void NgimuReceiveSetSensorsCallback(void (*newSensorsCallback)(const NgimuSensors ngimuSensors));
+void NgimuReceiveSetQuaternionCallback(void (*newQuaternionCallback)(const NgimuQuaternion ngimuQuaternion));
+void NgimuReceiveSetEulerCallback(void (*newEulerCallback)(const NgimuEuler ngimuEuler));
+void NgimuReceiveSetTemperatureCallback(void (*newTemperatureCallback)(const NgimuTemperature ngimuEuler));
+void NgimuReceiveProcessSerialByte(const char byte);
+void NgimuReceiveProcessUdpPacket(const char * const source, const size_t sourceSize);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+//------------------------------------------------------------------------------
+// End of file
